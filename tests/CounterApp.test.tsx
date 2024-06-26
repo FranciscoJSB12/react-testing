5f376e29d8c18031338503d525260f9f76bda8ee
FranciscoJSB12 <javif8@gmail.com>
2024-06-26 18:37:19 -0400

    test: Finish Counter App test

--- tests/CounterApp.test.tsx
+++ tests/CounterApp.test.tsx
@@ -1,5 +1,5 @@
 import { test, expect, describe, afterEach } from 'vitest';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, fireEvent } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Pruebas en <CounterApp />', () => {
@@ -16,5 +16,33 @@ describe('Pruebas en <CounterApp />', () => {
   test('Debe mostrar el valor inicial de 10', () => {
     render(<CounterApp value={initialValue} />);
     expect(screen.getByText(10));
+  });
+
+  test('Debe incrementar con el botón +1', () => {
+    render(<CounterApp value={initialValue} />);
+
+    fireEvent.click(screen.getByText('+1'));
+
+    expect(screen.getByText('11')).toBeTruthy();
+  });
+
+  test('Debe decrementar con el botón -1', () => {
+    render(<CounterApp value={initialValue} />);
+
+    fireEvent.click(screen.getByText('-1'));
+
+    expect(screen.getByText('9')).toBeTruthy();
+  });
+
+  test('Debe resetear la cuenta', () => {
+    render(<CounterApp value={355} />);
+
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    fireEvent.click(screen.getByText('+1'));
+    // fireEvent.click(screen.getByText('Reset'));
+    fireEvent.click(screen.getByRole('button', { name: 'btn-reset' }));
+
+    expect(screen.getByText(355)).toBeTruthy();
   });
 });
